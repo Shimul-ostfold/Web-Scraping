c416bebf03ca6ad60514afc77fe902537efb98f4
Mohammed Nurul Islam <mnis00014@gmail.com>
2024-04-17 19:44:54 +0600

    First scrapy page

--- bookscraper/bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,11 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+
+        for book in books:
+            yield{
+                'name'  :   book.css('h3 a::text').get(),
+                'price' :   book.css('div .price_color::text').get(),
+                'url'   :   book.css('div a').attrib['href'],
+            }
