863c0842fab2c7ffc87ca3ec738c232ebcad9767
Mohammed Nurul Islam <mnis00014@gmail.com>
2024-05-05 02:06:00 +0600

    new project

--- worldscraper/worldscraper/pipelines.py
+++ worldscraper/worldscraper/pipelines.py
@@ -8,6 +8,6 @@
 from itemadapter import ItemAdapter
 
 
-class SpiderTutorialPipeline:
+class WorldscraperPipeline:
     def process_item(self, item, spider):
         return item
--- worldscraper/worldscraper/settings.py
+++ worldscraper/worldscraper/settings.py
@@ -1,4 +1,4 @@
-# Scrapy settings for spider_tutorial project
+# Scrapy settings for worldscraper project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -7,14 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = "spider_tutorial"
+BOT_NAME = "worldscraper"
 
-SPIDER_MODULES = ["spider_tutorial.spiders"]
-NEWSPIDER_MODULE = "spider_tutorial.spiders"
+SPIDER_MODULES = ["worldscraper.spiders"]
+NEWSPIDER_MODULE = "worldscraper.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "spider_tutorial (+http://www.yourdomain.com)"
+#USER_AGENT = "worldscraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -45,13 +45,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    "spider_tutorial.middlewares.SpiderTutorialSpiderMiddleware": 543,
+#    "worldscraper.middlewares.WorldscraperSpiderMiddleware": 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    "spider_tutorial.middlewares.SpiderTutorialDownloaderMiddleware": 543,
+#    "worldscraper.middlewares.WorldscraperDownloaderMiddleware": 543,
 #}
 
 # Enable or disable extensions
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    "spider_tutorial.pipelines.SpiderTutorialPipeline": 300,
+#    "worldscraper.pipelines.WorldscraperPipeline": 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
